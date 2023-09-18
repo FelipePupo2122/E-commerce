3524dbccf99aade96e3be94f01defa25d570716b
Felipe Pupo A <79878769+FelipePupo2122@users.noreply.github.com>
2023-09-18 19:57:47 -0300

    Add files via upload
    Primeiro commit

---
+++ Migrations/20230904231601_NovaMigration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Ecommerce.Migrations
+{
+    public partial class NovaMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models 
+{
+    public class Produto 
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public double Preco { get; set; }
+        //Relação de Um para Muitos de produtos e categoria.
+        public Categoria Categoria {get;set;}
+        
+    }
+}
